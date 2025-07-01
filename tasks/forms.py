6569b7b52119139c05da1aa6c202ad5f1c5dd3ae
mohammad tahsin <mohammadtahsinhabib@gmail.com>
2025-07-01 22:56:32 +0600

    model design updated and database has been reset

--- tasks/forms.py
+++ tasks/forms.py
@@ -14,14 +14,51 @@ class TaskForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.fields['assigned_to'].choices = [(emp.id, emp.name) for emp in employees]
 
-class TaskModelForm(forms.ModelForm):
+
+
+
+
+class StyledFormMixin:
+    """ Mixing to apply style to form field"""
+
+    default_classes = "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
+
+    def apply_styled_widgets(self):
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class': self.default_classes,
+                    'placeholder': f"Enter {field.label.lower()}"
+                })
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class': f"{self.default_classes} resize-none",
+                    'placeholder':  f"Enter {field.label.lower()}",
+                    'rows': 5
+                })
+            elif isinstance(field.widget, forms.SelectDateWidget):
+                field.widget.attrs.update({
+                    "class": "border-2 border-gray-300 p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
+                })
+            elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                field.widget.attrs.update({
+                    'class': "space-y-2"
+                })
+            else:
+                field.widget.attrs.update({
+                    'class': self.default_classes
+                })
+
+
+class TaskModelForm(StyledFormMixin,forms.ModelForm):
     class Meta:
         model=Task
         fields=['title','descriptions','due_date','assigned_to']
         widgets={
-            "title":forms.TextInput(attrs={
-            class : "bordee-2 border-gray-300 w-full rounded-lg shadow-sm focus: border-rose-300","placeholder"="Enter..."})
-            "due_date":forms.SelectDateWidget(),
-            "assigned_to":forms.CheckboxSelectMultiple(),
+            "due_date":forms.SelectDateWidget,
+            "assigned_to":forms.CheckboxSelectMultiple,
         }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_styled_widgets()
 
--- tasks/models.py
+++ tasks/models.py
@@ -11,7 +11,12 @@ class Employee(models.Model):
         return self.name
 
 class Task(models.Model):
-    
+    STATUS_CHOICES=[
+        {"PENDING","Pending"},
+        {"IN_PROGRESS","Inp Progress"},
+        {"COMPLETED","Completed"}
+    ]
+
     project = models.ForeignKey("Project",on_delete=models.CASCADE,default=1)
     assigned_to = models.ManyToManyField(Employee)
 
@@ -19,8 +24,13 @@ class Task(models.Model):
     descriptions = models.TextField()
     due_date=models.DateField()
     is_completed = models.BooleanField(default=False)
+    status=models.CharField(max_length=15,choices=STATUS_CHOICES,default="PENDING")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
 
 
 class TaskDetails(models.Model):
@@ -34,8 +44,18 @@ class TaskDetails(models.Model):
     task=models.OneToOneField(Task,on_delete=models.CASCADE)
     assigned_to=models.CharField(max_length=250)
     priority=models.CharField(max_length=1,choices=PRIORITY_OPTIONS,default=LOW,)
+    notes=models.TextField(blank=True,null=True)
+
+
+    def __str__(self):
+        return f"Details form of {self.task.title}"
 
 class Project(models.Model):
     name =models.CharField(max_length=250)
+    description = models.TextField(blank=True,null=True)
     start_date = models.DateField()
+
+
+    def __str__(self):
+        return self.name
 
